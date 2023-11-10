f6806927256535b2688c377d9402b2968d5687fa
carlos.eduardo <carlos.eduardo@tce.ma.gov.br>
2023-11-10 11:29:27 -0300

    feat:fix fontawesome

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,26 @@ import './assets/Style/style.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
 
 import App from './App.vue'
 import router from './router'
 
 
-library.add(faUserSecret)
+// library.add(faUserSecret, faPhone, faRightToBracket)
 
 const app = createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
