676f1f0499796b0789ee4c233177169119fdd38a
kadu91878 <kadu_91878@hotmail.com>
2023-11-16 15:36:45 -0300

    feat: faq questions first part

--- src/components/FaqSection.vue
+++ src/components/FaqSection.vue
@@ -1,3 +1,45 @@
 <template>
-    
+    <div class="faq-page section" id="faq">
+        <div class="container">
+            <div class="head-title">
+                <div class="data-small-title"> Frquently Asked Questions</div>
+                Questions that are always asked
+            </div>
+            <div class="sub-title">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br>Lorem ipsum dolor sit amet consectetur.<br>Lorem ipsum dolor sit.
+            </div>
+
+            <div class="faq-container">
+                <div class="faq-item" id="quest1">
+                    <div class="title-quest" >
+                        <h2>can i rent a car at 18 yo?</h2>
+                        <a href="quest1"><i class="fa-solid fa-caret-down help-button"></i></a>
+                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                    </div>
+                </div>
+                <div class="faq-item" id="quest2">
+                    <div class="title-quest" >
+                        <h2>how do i change or cancel my reservation?</h2>
+                        <a href="quest2"><i class="fa-solid fa-caret-down help-button"></i></a>
+                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                    </div>
+                </div>
+                <div class="faq-item" id="quest3">
+                    <div class="tittle-quest" >
+                        <h2>will enterprise pick me up?</h2>
+                        <a href="quest3"><i class="fa-solid fa-caret-down help-button"></i></a>
+                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                    </div>
+                </div>
+                <div class="faq-item" id="quest4">
+                    <div class="title-quest" >
+                        <h2>does enterprise sell vehicles?</h2>
+                        <a href="quest4"><i class="fa-solid fa-caret-down help-button"></i></a>
+                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
