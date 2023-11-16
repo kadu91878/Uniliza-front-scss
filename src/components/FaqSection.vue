f0dbea44469afadfc7e7ef045ac08c0324e714e8
kadu91878 <kadu_91878@hotmail.com>
2023-11-16 16:05:27 -0300

    feat: add faq page part 2

--- src/components/FaqSection.vue
+++ src/components/FaqSection.vue
@@ -2,7 +2,7 @@
     <div class="faq-page section" id="faq">
         <div class="container">
             <div class="head-title">
-                <div class="data-small-title"> Frquently Asked Questions</div>
+                <div class="data-small-title"> Frequently Asked Questions</div>
                 Questions that are always asked
             </div>
             <div class="sub-title">
@@ -13,30 +13,30 @@
                 <div class="faq-item" id="quest1">
                     <div class="title-quest" >
                         <h2>can i rent a car at 18 yo?</h2>
-                        <a href="quest1"><i class="fa-solid fa-caret-down help-button"></i></a>
-                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                        <a href="#quest1"><i class="fa-solid fa-caret-down help-button"></i></a>
                     </div>
+                    <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
                 </div>
                 <div class="faq-item" id="quest2">
                     <div class="title-quest" >
                         <h2>how do i change or cancel my reservation?</h2>
-                        <a href="quest2"><i class="fa-solid fa-caret-down help-button"></i></a>
-                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                        <a href="#quest2"><i class="fa-solid fa-caret-down help-button"></i></a>
                     </div>
+                    <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
                 </div>
                 <div class="faq-item" id="quest3">
-                    <div class="tittle-quest" >
+                    <div class="title-quest" >
                         <h2>will enterprise pick me up?</h2>
-                        <a href="quest3"><i class="fa-solid fa-caret-down help-button"></i></a>
-                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                        <a href="#quest3"><i class="fa-solid fa-caret-down help-button"></i></a>
                     </div>
+                    <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
                 </div>
                 <div class="faq-item" id="quest4">
                     <div class="title-quest" >
                         <h2>does enterprise sell vehicles?</h2>
-                        <a href="quest4"><i class="fa-solid fa-caret-down help-button"></i></a>
-                        <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
+                        <a href="#quest4"><i class="fa-solid fa-caret-down help-button"></i></a>
                     </div>
+                    <div class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, mollitia. Voluptatem, nisi dolore, eum aut pariatur repudiandae repellat sapiente est vero vel blanditiis, minima sit ipsam facilis saepe sint qui.</div>
                 </div>
             </div>
         </div>
